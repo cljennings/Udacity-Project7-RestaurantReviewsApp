2e24306357eefd12b4bd7af239b518ab845d42ca
cljennings <46170669+cljennings@users.noreply.github.com>
2019-01-19 20:13:20 -0500

    ServiceWorker Successfully Connect

--- js/main.js
+++ js/main.js
@@ -6,13 +6,20 @@ var markers = []
 
 /**
  * Entry point to ServiceWorker
- */
- if('ServiceWorker' in Navigator){
-   navigator.serviceWorker.register('/Udacity-Project7-RestaurantReviewsApp/sw.js')
-   .then(function()=>console.log('success');)
-   .catch(function()=>console.log('fail');)
- }
 
+ if('ServiceWorker' in Navigator){
+   navigator.serviceWorker.register('/sw.js')
+//  .then(function(resolve) => console.log('success'))
+//  .catch(function(error) => console.log('fail'))
+ }
+*/
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(resolve) {console.log('Success');},
+     /*catch*/ function(reject) {console.log('fail');
+  });
+} 
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
---
+++ sw.js
@@ -0,0 +1,31 @@
+console.log('success');
+
+self.addEventListener('install',evt => {
+  const files = [
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'css/styles.css',
+    '/index.html',
+    'restaurant.html'
+  ];
+  evt.waitUntil(
+    caches.open('storage').then(function(cache){
+      cache.addAll(files);
+      })
+  )
+});
+
+self.addEventListener('fetch', evt => {
+
+});
